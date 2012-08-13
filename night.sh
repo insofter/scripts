7816a18c2216ed6ccaf26acb1bbf90162fb4a82d
Piotr Mika <piotr.mt.mika@gmail.com>
2012-08-13 14:40:05 +0200

    dodany beep

--- night.sh
+++ night.sh
@@ -24,12 +24,16 @@ echo "make 2>&1 | tee ../night_${DATE}.make_1.log ; test \${PIPESTATUS[0]} -eq 0
 
 
 
+sudo beep -l 1000
+sleep 1
+
 cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h
 echo  "cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h" >> ../night_${DATE}.all.log
 
 mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h
 echo "mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h" >> ../night_${DATE}.all.log
 
+sudo beep -l 1000
 
 
 
@@ -45,6 +49,14 @@ echo "make relpkg 2>&1 | tee ../night_${DATE}.make_relpkg.log ; test \${PIPESTAT
 
 echo DONE @ ${DATE}
 echo DONE @ ${DATE} >> ../night_${DATE}.all.log
+
+sudo beep -l 1000
+sleep 1
+sudo beep -l 1000
+sleep 1
+sudo beep -l 1000
+sleep 1
+sudo beep -l 1000
 
 sleep 30
 
