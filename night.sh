6312ac21db16c949b60be67eabe2217ba5898510
Piotr Mika <piotr.mt.mika@gmail.com>
2012-08-14 11:40:28 +0200

    zmieniony system logowania w night.sh i czas pocz i konca

--- night.sh
+++ night.sh
@@ -3,52 +3,48 @@
 DATE=`date +%Y.%m.%d__%H-%M-%S`
 
 cd /home/insofter/projects/buildroot
-echo -n "pwd: " 
-pwd
-echo -n "pwd: " >> ../night_${DATE}.all.log
-pwd >> ../night_${DATE}.all.log
+echo -n "pwd: `pwd`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
-
+#przenosimy stary output
 if [ -e output ] 
 then
   mv output __output__${DATE}
-  echo mv output __output__${DATE}
-  echo mv output __output__${DATE} >> ../night_${DATE}.all.log
+  echo mv output __output__${DATE} 2>&1 | tee -a ../night_${DATE}.all.log
 fi
 
 
-
+#kompilujemy całość do błedu z getsem
+echo "make_1 start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 make 2>&1 | tee ../night_${DATE}.make_1.log ; test ${PIPESTATUS[0]} -eq 0
-echo "make 2>&1 | tee ../night_${DATE}.make_1.log ; test \${PIPESTATUS[0]} -eq 0" >> ../night_${DATE}.all.log
-
+echo "make_1 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
 sudo beep -l 1000
 sleep 1
 
+#poprawiamy getsa (workaround)
 cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h
 echo  "cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h" >> ../night_${DATE}.all.log
-
 mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h
 echo "mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h" >> ../night_${DATE}.all.log
 
 sudo beep -l 1000
 
 
-
+#kompilujemy dalej
+echo "make_2 start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 make 2>&1 | tee ../night_${DATE}.make_2.log ; test ${PIPESTATUS[0]} -eq 0
-echo "make 2>&1 | tee ../night_${DATE}.make_2.log ; test \${PIPESTATUS[0]} -eq 0" >> ../night_${DATE}.all.log
+echo "make_2 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
-
+#budujemy pakiet
+echo "make_relpkg start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 make relpkg 2>&1 | tee ../night_${DATE}.make_relpkg.log ; test ${PIPESTATUS[0]} -eq 0
-echo "make relpkg 2>&1 | tee ../night_${DATE}.make_relpkg.log ; test \${PIPESTATUS[0]} -eq 0" >> ../night_${DATE}.all.log
+echo "make_relpkg end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
-
-echo DONE @ ${DATE}
-echo DONE @ ${DATE} >> ../night_${DATE}.all.log
+echo -e "DONE\nstart: ${DATE}\nend:`date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 sudo beep -l 1000
 sleep 1
