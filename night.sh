4cad61eb79b93639eec354c4a2f470e88d503996
Piotr Mika <piotr.mt.mika@gmail.com>
2012-08-20 12:54:09 +0200

    wysyłamy status po ssh

--- night.sh
+++ night.sh
@@ -16,35 +16,47 @@ fi
 
 #kompilujemy całość do błedu z getsem
 echo "make_1 start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
-make 2>&1 | tee ../night_${DATE}.make_1.log ; test ${PIPESTATUS[0]} -eq 0
-echo "make_1 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+make 2>&1 | tee ../night_${DATE}.make_1.log
+
+if [ ${PIPESTATUS[0]} -eq 0 ] #jesli 1 make nie poszło to próbujemy naprawic getsa i puszczamy jeszcze raz
+then
+  echo OK >> ../night_${DATE}.all.log 
+  echo "make_1 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+
+else
+
+  echo ERR >> ../night_${DATE}.all.log
+  echo "make_1 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
-sudo beep -l 1000
-sleep 1
+  sudo beep -l 1000
+  sleep 1
 
-#poprawiamy getsa (workaround)
-cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h
-echo  "cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h" >> ../night_${DATE}.all.log
-mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h
-echo "mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h" >> ../night_${DATE}.all.log
+  #poprawiamy getsa (workaround)
+  cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h
+  echo  "cat output/build/host-m4-1.4.15/lib/stdio.in.h | grep -v 'undef gets' | grep -v 'gets is a security hole' > .tmp.stdio.h" >> ../night_${DATE}.all.log
+  mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h
+  echo "mv .tmp.stdio.h output/build/host-m4-1.4.15/lib/stdio.in.h" >> ../night_${DATE}.all.log
 
-sudo beep -l 1000
+  sudo beep -l 1000
 
 
-#kompilujemy dalej
-echo "make_2 start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
-make 2>&1 | tee ../night_${DATE}.make_2.log ; test ${PIPESTATUS[0]} -eq 0
-echo "make_2 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+  #kompilujemy dalej
+  echo "make_2 start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+  make 2>&1 | tee ../night_${DATE}.make_2.log ; test ${PIPESTATUS[0]} -eq 0 && echo OK >> ../night_${DATE}.all.log || echo ERR >> ../night_${DATE}.all.log
+  echo "make_2 end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+
+fi #koniec proby naprawiania
+
 
 
 #budujemy pakiet
 echo "make_relpkg start: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
-make relpkg 2>&1 | tee ../night_${DATE}.make_relpkg.log ; test ${PIPESTATUS[0]} -eq 0
+make relpkg 2>&1 | tee ../night_${DATE}.make_relpkg.log ; test ${PIPESTATUS[0]} -eq 0 && echo OK >> ../night_${DATE}.all.log || echo ERR >> ../night_${DATE}.all.log
 echo "make_relpkg end: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 
-echo -e "DONE\nstart: ${DATE}\nend:`date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
+echo -e "END\nstart: ${DATE}\nend: `date +%Y.%m.%d__%H-%M-%S`" 2>&1 | tee -a ../night_${DATE}.all.log
 
 sudo beep -l 1000
 sleep 1
@@ -53,6 +65,13 @@ sleep 1
 sudo beep -l 1000
 sleep 1
 sudo beep -l 1000
+
+
+
+
+scp ../night_${DATE}.* ja@freeshell.de:
+
+
 
 sleep 30
 
