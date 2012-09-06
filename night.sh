b67a81fd9f68e796f15049ae724ffdb3ed940b7d
Piotr Mika <piotr.mt.mika@gmail.com>
2012-09-06 08:44:16 +0200

    tworzenie .config, gdy nie istnieje

--- night.sh
+++ night.sh
@@ -38,6 +38,14 @@ mkdir output
 echo -e ${DATE}__`git log --format=%s -1 | sed 's/ /_/g; s/[^a-zA-Z0-9_]//g; s/__/_/g'` \
   >> output/_about_me
 
+#.config
+if ! [ -e .config ] 
+then
+  make icdtcp3_defconfig
+  echo "make icdtcp3_defconfig" | tee -a ../night_${DATE}.all.log
+fi
+
+
 
 
 #kompilujemy całość do błedu z getsem
