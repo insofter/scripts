fc29bf1f5978e6ecc2ad5b6168574c3cfeb50c77
Piotr Mika <piotr.mt.mika@gmail.com>
2012-09-24 12:06:06 +0200

    auto-pull

---
+++ git_pull.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+cd /home/insofter/projects/icd
+
+if git status | grep  -q "nothing to commit" 
+then
+  git pull
+else
+  echo Commit icd!
+fi
+
+
+cd /home/insofter/projects/buildroot
+
+if git checkout icdtcp3-2011.11 && git status | grep  -q "nothing to commit"
+then
+  git pull
+else
+  echo Commit buildroot!
+fi
+
+cd /home/insofter/projects/scripts
+
+if git status | grep  -q "nothing to commit"
+then
+  git pull
+else
+  echo Commit scripts!
+fi
+
