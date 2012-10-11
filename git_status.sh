8e5dc01bc8f9a43db36f16c5de6c0df05dc3eb08
Piotr Mika <piotr.mt.mika@gmail.com>
2012-10-11 10:15:11 +0200

    status of all repos

---
+++ git_status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /home/insofter/projects/icd
+pwd
+git status
+echo
+echo
+
+cd /home/insofter/projects/buildroot
+pwd
+git status
+echo
+echo
+
+cd /home/insofter/projects/scripts
+pwd
+git status
+echo
+echo
+
+cd /home/insofter/projects/factory
+pwd
+git status
+echo
+echo
