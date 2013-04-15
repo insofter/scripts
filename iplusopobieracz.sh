48ef9bf179ab07379262dac090c95207eceeae89
Piotr Mika <piotr.mt.mika@gmail.com>
2013-04-15 11:07:02 +0200

    changed icdtcp3-uImage-prog for ip 192.168.123.7X

--- iplusopobieracz.sh
+++
@@ -1,9 +0,0 @@
-#wget http://ubuntu.task.gda.pl/ubuntu-releases//lucid/ubuntu-10.04.3-server-amd64.iso
-#wget http://ubuntu.task.gda.pl/ubuntu-releases//lucid/ubuntu-10.04.3-server-i386.iso
-#wget ftp://ftp.tpnet.pl/pub/linux/ubuntu-releases//oneiric/ubuntu-11.10-server-amd64.iso
-#wget ftp://ftp.tpnet.pl/pub/linux/ubuntu-releases//oneiric/ubuntu-11.10-desktop-i386.iso
-apt-get update
-apt-get -y -d upgrade
-shutdown -h now
-
-
--- s3fs-mount.sh
+++
@@ -1,1 +0,0 @@
-s3fs bfy59grjycv5 /media/s3fs/ -o url=https://s3.amazonaws.com
